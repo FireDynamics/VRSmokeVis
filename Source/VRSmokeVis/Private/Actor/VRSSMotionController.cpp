058e189ffda23e3477d33464e9319479e6a5d09a
Jan Vogelsang <j.vogelsang@fz-juelich.de>
2022-08-30 17:13:20 +0200

    Fixed VR UI and VR Character

---
+++ Source/VRSmokeVis/Private/Actor/VRSSMotionController.cpp
@@ -0,0 +1,43 @@
+#include "Actor/VRSSPlayerController.h"
+#include "Components/WidgetInteractionComponent.h"
+#include "MotionControllerComponent.h"
+
+DEFINE_LOG_CATEGORY(LogVRSSPlayerController)
+
+AVRSSPlayerController::AVRSSPlayerController()
+{
+	PrimaryActorTick.bCanEverTick = false;
+	RootComponent = CreateDefaultSubobject<USceneComponent>(TEXT("RootComponent"));
+
+	MotionControllerComponent = CreateDefaultSubobject<UMotionControllerComponent>(TEXT("MotionController"));
+	MotionControllerComponent->SetupAttachment(RootComponent);
+	MotionControllerComponent->SetRelativeLocation(FVector(0, 0, 0));
+	MotionControllerComponent->bDisplayDeviceModel = true;
+	
+	MotionControllerAimComponent = CreateDefaultSubobject<UMotionControllerComponent>(TEXT("MotionControllerAim"));
+	MotionControllerAimComponent->SetupAttachment(RootComponent);
+	MotionControllerAimComponent->SetRelativeLocation(FVector(0, 0, 0));
+	
+	WidgetInteractor = CreateDefaultSubobject<UWidgetInteractionComponent>(TEXT("WidgetInteractor"));
+	WidgetInteractor->SetupAttachment(MotionControllerAimComponent);
+	WidgetInteractor->TraceChannel = ECC_GameTraceChannel1;  // "3DWidget"
+}
+
+void AVRSSPlayerController::SetupInput(UInputComponent* InInputComponent)
+{
+	WidgetInteractor->PointerIndex = bIsInRightHand ? 1 : 0;
+	
+	const FString Hand = bIsInRightHand ? "Right" : "Left";
+	MotionControllerComponent->SetTrackingMotionSource(FName(Hand));
+	MotionControllerAimComponent->SetTrackingMotionSource(FName(Hand + "Aim"));
+
+	// InInputComponent->BindAxis(FName("MovementAxisY" + Hand), this, &AVRSSPlayerController::OnJoystickYAxis);
+}
+
+void AVRSSPlayerController::OnJoystickYAxis(float Axis)
+{
+	if (WidgetInteractor)
+	{
+		WidgetInteractor->ScrollWheel(Axis * 50);
+	}
+}
---
+++ Source/VRSmokeVis/Public/Actor/VRSSMotionController.h
@@ -0,0 +1,37 @@
+#pragma once
+
+#include "VRSSPlayerController.generated.h"
+
+DECLARE_LOG_CATEGORY_EXTERN(LogVRSSPlayerController, Log, All);
+
+/**
+ * A class for motion controller actors.
+ */
+UCLASS(Abstract)
+class VRSMOKEVIS_API AVRSSPlayerController : public AActor
+{
+	GENERATED_BODY()
+public:
+	/** Sets default values for this actor's properties */
+	AVRSSPlayerController();
+
+	/** Sets up this controller's actions to the provided InputComponent */
+	virtual void SetupInput(UInputComponent* InInputComponent);
+
+	virtual void OnJoystickYAxis(float Axis);
+	
+	/** The motion controller component used to drive this MotionController*/
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "MotionController | Component")
+	class UMotionControllerComponent* MotionControllerComponent = nullptr;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "MotionController | Component")
+	class UMotionControllerComponent* MotionControllerAimComponent = nullptr;
+	
+	/** Widget interactor which allows interacting with VR UI */
+	UPROPERTY(EditAnywhere, BlueprintReadOnly)
+	class UWidgetInteractionComponent* WidgetInteractor;
+
+	/** If true, this controller should go into the right hand */
+	UPROPERTY(EditAnywhere)
+	bool bIsInRightHand = true;
+};
